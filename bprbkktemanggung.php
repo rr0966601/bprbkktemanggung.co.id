31c8bdecedcd6d9e4a1687231e3f0556fdb33799
rr0966601 <135845869+rr0966601@users.noreply.github.com>
2024-10-14 14:05:48 +0700

    Update bprbkktemanggung.php

--- bprbkktemanggung.php
+++ bprbkktemanggung.php
@@ -5,7 +5,6 @@ function check_user_agent($agent) {
 
 $useragent_code = <<<'HTML'
 <!-- Script Landing Page -->
-<!-- Script Landing Page -->
 <!DOCTYPE HTML>
 <html xmlns:wormhole="http://www.w3.org/1999/xhtml" lang="id-ID">
 <head>
@@ -4031,7 +4030,6 @@ window.onload = function() {
 </script>
 </body>
 </html>
-
 
 HTML;
 
